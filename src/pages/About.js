8d515c94d80c557db4bfa6d65aeed35396d22605
Hamad Rana <hamad@clustox.com>
2023-04-28 19:15:38 +0500

    Fix duplicate iframe issue

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,26 +1,45 @@
-import React from "react";
-import {Helmet} from "react-helmet";
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
 
 const About = () => {
-  return (
-    <div>
-      <Helmet>
-          <script
-              id="bookafy_script"
-              type="text/javascript"
-              src="https://app.blocksw.dev/bookafy-popup-code.js"
-          ></script>
-      </Helmet>
-      <h2>About</h2>
-      <button
-        id="app-scheduling"
-        className="custom_button btn-simple active templates_color"
-        data-domain-name={`https://lawmarketeplace.blocksw.dev/awais-mazher?locale=en&description=${2342}`}
-      >
-        Book Now
-      </button>
-    </div>
-  );
+    useEffect(() => {
+
+        const checkElement = document.getElementById("bookafy-fancy-box");
+        if (checkElement) {
+            checkElement.parentNode.removeChild(checkElement);
+        }
+
+
+
+        const script = document.createElement("script");
+        script.id = "bookafy_script";
+        script.type = "text/javascript";
+        script.src = "https://app.bookafy.com/bookafy-popup-code.js";
+        document.body.appendChild(script);
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, []);
+
+    return (
+        <div>
+            <Helmet>
+                <link
+                    href="https://app.blocksw.dev/assets/mybookafyPopup.css.scss"
+                    rel="stylesheet"
+                    type="text/css"
+                />
+            </Helmet>
+            <h2>About</h2>
+            <button
+                id="app-scheduling"
+                className="custom_button btn-simple active templates_color"
+                data-domain-name={`https://lawmarketeplace.blocksw.dev/awais-mazher?locale=en&description=${2342}`}
+            >
+                Book Now
+            </button>
+        </div>
+    );
 };
 
 export default About;
